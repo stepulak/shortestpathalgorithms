36f62b7a3b415955912c61f636fb0741129a735b
Štěpán Trčka <savage2@email.cz>
2024-06-06 22:47:54 +0200

    a_star refactor, a_star use binary heap

--- a_star.go
+++ a_star.go
@@ -6,80 +6,58 @@ import (
 	"slices"
 )
 
-func infScore(m map[int]float64, idx int) float64 {
-	v, ok := m[idx]
-	if !ok {
-		return math.Inf(0)
-	}
-	return v
-}
-
 func (g *Graph[TValue]) AStar(start, end *Node[TValue], h func(n *Node[TValue]) float64) (NodeList[TValue], error) {
 	if start == nil || end == nil {
 		return nil, fmt.Errorf("invalid start or end")
 	}
-	openSet := []*Node[TValue]{start}
-	cameFrom := map[int]*Node[TValue]{}
-	idToNode := map[int]*Node[TValue]{
-		start.Id: start,
-		end.Id:   end,
-	}
-	gScore := map[int]float64{}
-	gScore[start.Id] = 0
+	type nodeScore map[*Node[TValue]]float64
 
-	fScore := map[int]float64{}
-	fScore[start.Id] = h(start)
+	gScore := nodeScore{start: 0}
+	fScore := nodeScore{start: h(start)}
+	cameFrom := map[*Node[TValue]]*Node[TValue]{}
 
-	lowestFscore := func() int {
-		best := fScore[openSet[0].Id]
-		bestI := openSet[0].Id
-		for i := range openSet {
-			if infScore(fScore, openSet[i].Id) < best {
-				bestI = i
-				best = infScore(fScore, i)
-			}
+	scoreOrInf := func(scoreMap nodeScore, n *Node[TValue]) float64 {
+		val, ok := scoreMap[n]
+		if !ok {
+			return math.Inf(0)
 		}
-		return bestI
+		return val
 	}
-	reconstructPath := func(current int) NodeList[TValue] {
-		curr, ok := idToNode[current]
+
+	reconstructPath := func(current *Node[TValue]) NodeList[TValue] {
 		totalPath := NodeList[TValue]{}
-		for ok {
-			totalPath = append(totalPath, curr)
-			curr, ok = cameFrom[curr.Id]
+		for {
+			totalPath = append(totalPath, current)
+			next, ok := cameFrom[current]
+			if !ok {
+				break
+			}
+			current = next
 		}
 		slices.Reverse(totalPath)
 		return totalPath
 	}
 
-	for len(openSet) > 0 {
-		current := lowestFscore()
-		if current == end.Id {
+	openSet := NewBinaryHeap[*Node[TValue]](func(a, b *Node[TValue]) bool {
+		return scoreOrInf(fScore, a) < scoreOrInf(fScore, b)
+	})
+	openSet.Push(start)
+
+	for openSet.Size() > 0 {
+		current, _ := openSet.Pop()
+		if current == end {
 			return reconstructPath(current), nil
 		}
-		for i, v := range openSet {
-			if v.Id == current {
-				openSet = append(openSet[:i], openSet[i+1:]...)
-				break
-			}
-		}
-		for _, neighbour := range idToNode[current].Edges {
-			idToNode[neighbour.To.Id] = neighbour.To
+		for _, neighbour := range current.Edges {
 			tentativeGScore := gScore[current] + neighbour.Distance
-			if tentativeGScore < infScore(gScore, neighbour.To.Id) {
-				cameFrom[neighbour.To.Id] = idToNode[current]
-				gScore[neighbour.To.Id] = tentativeGScore
-				fScore[neighbour.To.Id] = tentativeGScore + h(neighbour.To)
-				inSet := false
-				for _, v := range openSet {
-					if neighbour.To.Id == v.Id {
-						inSet = true
-						break
-					}
-				}
-				if !inSet {
-					openSet = append(openSet, neighbour.To)
-				}
+			if tentativeGScore >= scoreOrInf(gScore, neighbour.To) {
+				continue
+			}
+			cameFrom[neighbour.To] = current
+			gScore[neighbour.To] = tentativeGScore
+			fScore[neighbour.To] = tentativeGScore + h(neighbour.To)
+			if !openSet.Contains(neighbour.To, func(a, b *Node[TValue]) bool { return a == b }) {
+				openSet.Push(neighbour.To)
 			}
 		}
 	}
